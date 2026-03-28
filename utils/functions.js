ed419ef2c7e7bbf45b0278397aab43a554464241
Shubham Meena <shubhamm.bca2021@ssism.org>
2026-03-28 18:03:32 +0530

    removed commented code.

--- utils/functions.js
+++ utils/functions.js
@@ -1,11 +1,7 @@
-// utils/pagination.js
-
 const getPaginatedData = async ({ model, query, page, per_page, populate = "", sort = { createdAt: -1 } }) => {
   const currentPage = parseInt(page) || 1;
   const limit = parseInt(per_page) || 10;
   const skip = (currentPage - 1) * limit;
-
-  // Ek saath data aur total count nikalne ke liye Promise.all best hai (Performance boost)
   const [items, total] = await Promise.all([
     model.find(query).populate(populate).sort(sort).skip(skip).limit(limit),
     model.countDocuments(query),

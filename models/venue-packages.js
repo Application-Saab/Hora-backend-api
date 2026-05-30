8e8e26594b78c29fef1aa8bc13c654e1ed0a55c2
Shubham Meena <shubhamm.bca2021@ssism.org>
2026-05-30 19:23:01 +0530

    add venue schema model for MongoDB

---
+++ models/venue-packages.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const venueSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+
+    venueType: { type: String, default: "" },
+    venueName: { type: String, default: "" },
+    location: { type: String, default: "" },
+    googleMapLink: { type: String, default: "" },
+  },
+  {
+    strict: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Venues", venueSchema);
